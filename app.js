8c41d9e487ffbb9f5b13a9f5434de8f685390d89
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-09-18 18:12:18 +0200

    blabla

--- app.js
+++ app.js
@@ -1,47 +1,56 @@
-fetch("pokemon.json")
+
+fetch("100.json")
   .then((response) => response.json())
   .then((pokeBase) => {
     for (pokemon of pokeBase) {
-      document.body.innerHTML += 
+      var typeUn = pokemon.apiTypes[0].name;
+      var typeDeux = pokemon.apiTypes[1].name;
+      console.log(typeUn);
+      console.log(typeDeux);
+      
+        document.body.innerHTML += 
       `
-      <div class="pokemon">
-      <div class="border">
+      <div class="card">
         <h2>${pokemon.name}</h2>
-        <h3>Pokedex #${pokemon.id}</h3>
+        <h3>Pokedex ID# ${pokemon.id}</h3>
         <div class="type">
-        <img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
-        <p>${pokemon.apiTypes[0].name}</p>
-        <img class="typeImage" src="${pokemon.apiTypes[1].image}"/>
-        <p>${pokemon.apiTypes[1].name}</p>
+          <img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
+          <p>${pokemon.apiTypes[0].name}</p>
         </div>
-        
-        
         <img class="imgPoke" src="${pokemon.image}">
-        
-      </div>    
       </div>
-      `;
-    }
-
+      `
+      ;
     
-  });
+  }
+}
+
+
+);//---------------------------FIN DE THEN ----------------------------
 
 
 
-  // for (pokemon of pokeBase) {
-  //   document.body.innerHTML += 
-  //   `
-  //   <div class="pokemon">
-  //   <div class="border">
-  //     <h2>${pokemon.name}</h2>
-  //     <p>Pokedex #${pokemon.id}</p>
-  //     <img src="${pokemon.image}">
-  //     <p>${pokemon.apiTypes.entries[1]}</p>
-  //   </div>    
-  //   </div>
-  //   `;
-  // }
-  // ${pokemon.apiTypes[1].name}
 
-  // <h2>${pokeBase[1].apiTypes[1].name}</h2>
-  // <h2>${pokeBase[1].apiTypes[0].name}</h2>
+
+// let pokeImg = document.querySelectorAll(".imgPoke");
+// let modal = document.querySelector(".modal");
+
+
+
+// pokeImg.forEach((item)=>{
+//   item.onclick = function(){
+//     modal.classList.add("active");
+//     modal.innerHTML += `<img class="imgModal" src="${pokemon.image}"> `;
+    
+//   }
+ 
+// })
+
+// const imgModal = document.querySelector(".imgModal");
+
+// window.onclick = function (e) {
+//   console.log(e.target)
+//   if (e.target == imgModal) {
+//     modal.classList.remove("active");
+//   }
+// };
