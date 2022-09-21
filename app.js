b433a9032293a69f3716e8e3b893cd33fbe9dc69
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-09-21 15:00:47 +0200

    fonction

--- app.js
+++ app.js
@@ -1,76 +1,73 @@
+let typeCouleur = {
+  Normal:
+    "background: rgb(184,184,184);background: linear-gradient(50deg, rgba(184,184,184,1) 30%, rgba(255,255,255,1) 50%, rgba(184,184,184,1) 70%)",
+  Combat:
+    "background: rgb(255,122,122);background: linear-gradient(50deg, rgba(255,122,122,1) 30%, rgba(255,0,0,1) 50%, rgba(255,122,122,1) 70%)",
+  Vol: "background: rgb(190,255,254);background: linear-gradient(50deg, rgba(190,255,254,1) 30%, rgba(46,254,245,1) 50%, rgba(190,255,254,1) 70%);",
+  Poison:
+    "background: rgb(239,0,255);background: linear-gradient(50deg, rgba(239,0,255,1) 30%, rgba(154,0,255,1) 50%, rgba(239,0,255,1) 70%);",
+  Sol: "background: rgb(208,160,129);background: linear-gradient(50deg, rgba(208,160,129,1) 30%, rgba(195,101,41,1) 50%, rgba(208,160,129,1) 70%);",
+  Roche:
+    "background: rgb(255,205,111);background: linear-gradient(50deg, rgba(255,205,111,1) 30%, rgba(255,174,23,1) 50%, rgba(255,205,111,1) 70%)",
+  Insecte:
+    "background: rgb(169,255,135);background: linear-gradient(50deg, rgba(169,255,135,1) 30%, rgba(15,173,0,1) 50%, rgba(169,255,135,1) 70%)",
+  Spectre:
+    "background: rgb(166,95,232);background: linear-gradient(50deg, rgba(166,95,232,1) 30%, rgba(183,0,237,1) 50%, rgba(166,95,232,1) 70%)",
+  Acier:
+    "background: rgb(88,175,153);background: linear-gradient(50deg, rgba(88,175,153,1) 30%, rgba(114,209,215,1) 50%, rgba(88,175,153,1) 70%)",
+  Feu: "background: rgb(255,231,0);background: linear-gradient(50deg, rgba(255,231,0,1) 30%, rgba(255,141,0,1) 50%, rgba(255,231,0,1) 70%);",
+  Eau: "background: rgb(0,212,247);background: linear-gradient(50deg, rgba(0,212,247,1) 30%, rgba(0,116,255,1) 50%, rgba(0,212,247,1) 70%);",
+  Plante:
+    "background: rgb(0,191,93);background: linear-gradient(50deg, rgba(0,191,93,1) 30%, rgba(17,136,0,1) 50%, rgba(0,191,93,1) 70%);",
+  Électrik:
+    "background: rgb(221,221,221);background: linear-gradient(50deg, rgba(221,221,221,1) 30%, rgba(254,255,0,1) 50%, rgba(221,221,221,1) 70%)",
+  Psy: "background: rgb(255,49,88);background: linear-gradient(50deg, rgba(255,49,88,1) 30%, rgba(255,190,202,1) 50%, rgba(255,49,88,1) 70%)",
+  Glace:
+    "background: rgb(49,181,255);background: linear-gradient(50deg, rgba(49,181,255,1) 30%, rgba(190,252,255,1) 50%, rgba(49,181,255,1) 70%)",
+  Dragon:
+    "background: rgb(0,129,201);background: linear-gradient(50deg, rgba(0,129,201,1) 30%, rgba(2,0,163,1) 50%, rgba(0,129,201,1) 70%)",
+  Ténèbres:
+    "background: rgb(0,94,147);background: linear-gradient(50deg, rgba(0,94,147,1) 30%, rgba(1,0,59,1) 50%, rgba(0,94,147,1) 70%)",
+  Fée: "background: rgb(255,117,231);background: linear-gradient(50deg, rgba(255,117,231,1) 30%, rgba(255,200,246,1) 50%, rgba(255,117,231,1) 70%)",
+};
+// console.log(Object.keys(typeCouleur));
 fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/200")
   .then((response) => response.json())
   .then((pokebase) => {
     for (pokemon of pokebase) {
-      document.body.innerHTML += `
-        <div class="card" style="background: ${bgType1()}">
+      let main = document.querySelector("main");
+      main.innerHTML += `
+        <div class="card" style="background: ${bgType1(pokemon)}">
+          <img class="imgPoke" src="${pokemon.image}">
           <h2>${pokemon.name}</h2>
           <h3>#${pokemon.id}</h3>
-          <img class="imgPoke" src="${pokemon.image}">
-          <div class="type">${type()}
+          <div class="type">${type(pokemon)}
           </div>
         </div>
         `;
     }
-    function type() {
-      if (pokemon.apiTypes[1] == undefined) {
-        return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
-                  <p>${pokemon.apiTypes[0].name}</p>`;
-      } else {
-        return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
-                  <p>${pokemon.apiTypes[0].name}</p> <img class="typeImage" src="${pokemon.apiTypes[1].image}"/>
-                  <p>${pokemon.apiTypes[1].name}</p>`;
-      }
-    }
-    function bgType1() {
-      if (pokemon.apiTypes[1] == undefined) {
-        if (pokemon.apiTypes[0].name == "Poison")
-          return "background: rgb(239,0,255);background: linear-gradient(50deg, rgba(239,0,255,1) 30%, rgba(154,0,255,1) 50%, rgba(239,0,255,1) 70%);";
-        if (pokemon.apiTypes[0].name == "Plante")
-          return "background: rgb(0,191,93);background: linear-gradient(50deg, rgba(0,191,93,1) 30%, rgba(17,136,0,1) 50%, rgba(0,191,93,1) 70%);";
-        if (pokemon.apiTypes[0].name == "Feu")
-          return "background: rgb(255,231,0);background: linear-gradient(50deg, rgba(255,231,0,1) 30%, rgba(255,141,0,1) 50%, rgba(255,231,0,1) 70%);";
-        if (pokemon.apiTypes[0].name == "Eau")
-          return "background: rgb(0,212,247);background: linear-gradient(50deg, rgba(0,212,247,1) 30%, rgba(0,116,255,1) 50%, rgba(0,212,247,1) 70%);";
-        if (pokemon.apiTypes[0].name == "Insecte")
-          return "background: rgb(169,255,135);background: linear-gradient(50deg, rgba(169,255,135,1) 30%, rgba(15,173,0,1) 50%, rgba(169,255,135,1) 70%);";
-        if (pokemon.apiTypes[0].name == "Acier") return "lightgrey";
-        if (pokemon.apiTypes[0].name == "Combat") return "red";
-        if (pokemon.apiTypes[0].name == "Électrik") return "yellow";
-        if (pokemon.apiTypes[0].name == "Dragon") return "darkblue";
-        if (pokemon.apiTypes[0].name == "Fée") return "lightpink";
-        if (pokemon.apiTypes[0].name == "Glace") return "lightblue";
-        if (pokemon.apiTypes[0].name == "Normal") return "white";
-        if (pokemon.apiTypes[0].name == "Psy") return "pink";
-        if (pokemon.apiTypes[0].name == "Roche") return "gray";
-        if (pokemon.apiTypes[0].name == "Spectre") return "lightgray";
-        if (pokemon.apiTypes[0].name == "Sol") return "sand";
-        if (pokemon.apiTypes[0].name == "Vol") return "blue";
-        if (pokemon.apiTypes[0].name == "Ténèbres") return "black";
-      } else if (pokemon.apiTypes[1] !== undefined) {
-        if (pokemon.apiTypes[1].name == "Poison")
-          return "background: rgb(239,0,255);background: linear-gradient(50deg, rgba(239,0,255,1) 30%, rgba(154,0,255,1) 50%, rgba(239,0,255,1) 70%);";
-        if (pokemon.apiTypes[1].name == "Plante")
-          return "background: rgb(0,191,93);background: linear-gradient(50deg, rgba(0,191,93,1) 30%, rgba(17,136,0,1) 50%, rgba(0,191,93,1) 70%);";
-        if (pokemon.apiTypes[1].name == "Feu")
-          return "background: rgb(255,231,0);background: linear-gradient(50deg, rgba(255,231,0,1) 30%, rgba(255,141,0,1) 50%, rgba(255,231,0,1) 70%);";
-        if (pokemon.apiTypes[1].name == "Eau")
-          return "background: rgb(0,212,247);background: linear-gradient(50deg, rgba(0,212,247,1) 30%, rgba(0,116,255,1) 50%, rgba(0,212,247,1) 70%);";
-        if (pokemon.apiTypes[1].name == "Insecte")
-          return "background: rgb(169,255,135);background: linear-gradient(50deg, rgba(169,255,135,1) 30%, rgba(15,173,0,1) 50%, rgba(169,255,135,1) 70%);";
-        if (pokemon.apiTypes[1].name == "Acier") return "lightgrey";
-        if (pokemon.apiTypes[1].name == "Combat") return "red";
-        if (pokemon.apiTypes[1].name == "Électrik") return "yellow";
-        if (pokemon.apiTypes[1].name == "Dragon") return "darkblue";
-        if (pokemon.apiTypes[1].name == "Fée") return "lightpink";
-        if (pokemon.apiTypes[1].name == "Glace") return "lightblue";
-        if (pokemon.apiTypes[1].name == "Normal") return "white";
-        if (pokemon.apiTypes[1].name == "Psy") return "pink";
-        if (pokemon.apiTypes[1].name == "Roche") return "gray";
-        if (pokemon.apiTypes[1].name == "Spectre") return "lightgray";
-        if (pokemon.apiTypes[1].name == "Sol") return "sand";
-        if (pokemon.apiTypes[1].name == "Vol") return "blue";
-        if (pokemon.apiTypes[1].name == "Ténèbres") return "yellow";
-      }
-    }
   });
+
+function type(pokemon) {
+  if (pokemon.apiTypes[1] == undefined) {
+    return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
+                <p>${pokemon.apiTypes[0].name}</p>`;
+  } else {
+    return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
+                <p>${pokemon.apiTypes[0].name}</p> <img class="typeImage" src="${pokemon.apiTypes[1].image}"/>
+                <p>${pokemon.apiTypes[1].name}</p>`;
+  }
+}
+
+// function bgType1(pokemon) {
+
+for (const [key, value] of Object.entries(typeCouleur)) {
+  console.log(`${key}: ${value}`);
+}
+if (pokemon.apiTypes[1] == undefined) {
+  if (pokemon.apiTypes[0].name == Object.keys(typeCouleur)) console.log("ok");
+} else if (pokemon.apiTypes[1] !== undefined) {
+  if (pokemon.apiTypes[1].name == "Feu");
+}
+
+// }
