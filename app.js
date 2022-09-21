8782425a440b69ce53b7c145d93c6c186d3a5fca
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-09-21 08:18:27 +0200

    ajout couleurs

--- app.js
+++ app.js
@@ -1,73 +1,66 @@
-
-fetch("100.json")
+fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/200")
   .then((response) => response.json())
-  .then((pokeBase) => {
-    for (pokemon of pokeBase) {
-      
-    
-      if (pokemon.apiTypes[1].name == undefined){
-       
-        document.body.innerHTML += 
-        `
-        <div class="card">
+  .then((pokebase) => {
+    for (pokemon of pokebase) {
+      document.body.innerHTML += `
+        <div class="card" style="background: ${bgType1()}">
           <h2>${pokemon.name}</h2>
-          <h3>Pokedex ID# ${pokemon.id}</h3>
-          <div class="type">
-            <img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
-            <p>${pokemon.apiTypes[0].name}</p>
-          </div>
+          <h3>#${pokemon.id}</h3>
           <img class="imgPoke" src="${pokemon.image}">
-        </div>
-        `
-        ;
-      }
-      else{
-        document.body.innerHTML += 
-        `
-        <div class="card">
-          <h2>${pokemon.name}</h2>
-          <h3>Pokedex ID# ${pokemon.id}</h3>
-          <div class="type">
-            <img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
-            <p>${pokemon.apiTypes[0].name}</p>
-            <img class="typeImage" src="${pokemon.apiTypes[1].image}"/>
-            <p>${pokemon.apiTypes[1].name}</p>
+          <div class="type">${type()}
           </div>
-          <img class="imgPoke" src="${pokemon.image}">
         </div>
-        `
+        `;
+    }
+    function type() {
+      if (pokemon.apiTypes[1] == undefined) {
+        return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
+                  <p>${pokemon.apiTypes[0].name}</p>`;
+      } else {
+        return `<img class="typeImage" src="${pokemon.apiTypes[0].image}"/>
+                  <p>${pokemon.apiTypes[0].name}</p> <img class="typeImage" src="${pokemon.apiTypes[1].image}"/>
+                  <p>${pokemon.apiTypes[1].name}</p>`;
       }
-        
-    
-  }
-}
-
-
-);//---------------------------FIN DE THEN ----------------------------
-
-
-
-
-
-// let pokeImg = document.querySelectorAll(".imgPoke");
-// let modal = document.querySelector(".modal");
-
-
-
-// pokeImg.forEach((item)=>{
-//   item.onclick = function(){
-//     modal.classList.add("active");
-//     modal.innerHTML += `<img class="imgModal" src="${pokemon.image}"> `;
-    
-//   }
- 
-// })
-
-// const imgModal = document.querySelector(".imgModal");
-
-// window.onclick = function (e) {
-//   console.log(e.target)
-//   if (e.target == imgModal) {
-//     modal.classList.remove("active");
-//   }
-// };
+    }
+    function bgType1() {
+      if (pokemon.apiTypes[1] == undefined) {
+        if (pokemon.apiTypes[0].name == "Poison") return "purple";
+        if (pokemon.apiTypes[0].name == "Plante") return "green";
+        if (pokemon.apiTypes[0].name == "Feu") return "orange";
+        if (pokemon.apiTypes[0].name == "Eau") return "blue";
+        if (pokemon.apiTypes[0].name == "Insecte") return "lightgreen";
+        if (pokemon.apiTypes[0].name == "Acier") return "lightgrey";
+        if (pokemon.apiTypes[0].name == "Combat") return "red";
+        if (pokemon.apiTypes[0].name == "Électrik") return "yellow";
+        if (pokemon.apiTypes[0].name == "Dragon") return "darkblue";
+        if (pokemon.apiTypes[0].name == "Fée") return "lightpink";
+        if (pokemon.apiTypes[0].name == "Glace") return "lightblue";
+        if (pokemon.apiTypes[0].name == "Normal") return "white";
+        if (pokemon.apiTypes[0].name == "Psy") return "pink";
+        if (pokemon.apiTypes[0].name == "Roche") return "gray";
+        if (pokemon.apiTypes[0].name == "Spectre") return "lightgray";
+        if (pokemon.apiTypes[0].name == "Sol") return "sand";
+        if (pokemon.apiTypes[0].name == "Vol") return "blue";
+        if (pokemon.apiTypes[0].name == "Ténèbres") return "black";
+      } else if (pokemon.apiTypes[1] !== undefined) {
+        if (pokemon.apiTypes[1].name == "Poison") return "purple";
+        if (pokemon.apiTypes[1].name == "Plante") return "green";
+        if (pokemon.apiTypes[1].name == "Feu") return "orange";
+        if (pokemon.apiTypes[1].name == "Eau") return "blue";
+        if (pokemon.apiTypes[1].name == "Insecte") return "lightgreen";
+        if (pokemon.apiTypes[1].name == "Acier") return "lightgrey";
+        if (pokemon.apiTypes[1].name == "Combat") return "red";
+        if (pokemon.apiTypes[1].name == "Électrik") return "yellow";
+        if (pokemon.apiTypes[1].name == "Dragon") return "darkblue";
+        if (pokemon.apiTypes[1].name == "Fée") return "lightpink";
+        if (pokemon.apiTypes[1].name == "Glace") return "lightblue";
+        if (pokemon.apiTypes[1].name == "Normal") return "white";
+        if (pokemon.apiTypes[1].name == "Psy") return "pink";
+        if (pokemon.apiTypes[1].name == "Roche") return "gray";
+        if (pokemon.apiTypes[1].name == "Spectre") return "lightgray";
+        if (pokemon.apiTypes[1].name == "Sol") return "sand";
+        if (pokemon.apiTypes[1].name == "Vol") return "blue";
+        if (pokemon.apiTypes[1].name == "Ténèbres") return "yellow";
+      }
+    }
+  });
