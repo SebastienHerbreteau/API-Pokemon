dc7213dee15f73a4f32395dbcf55c5d102aff86d
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2022-09-21 19:56:01 +0200

    color

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 let typeCouleur = {
-  Normal:
-    "background: rgb(184,184,184);background: linear-gradient(50deg, rgba(184,184,184,1) 30%, rgba(255,255,255,1) 50%, rgba(184,184,184,1) 70%)",
+  Normal: "background-image: url('assets/normal.png')",
   Combat: "background-image: url('assets/combat.png')",
-  Vol: "background: rgb(190,255,254);background: linear-gradient(50deg, rgba(190,255,254,1) 30%, rgba(46,254,245,1) 50%, rgba(190,255,254,1) 70%);",
-  Poison:
-    "background: rgb(239,0,255);background: linear-gradient(50deg, rgba(239,0,255,1) 30%, rgba(154,0,255,1) 50%, rgba(239,0,255,1) 70%);",
-  Sol: "background: rgb(208,160,129);background: linear-gradient(50deg, rgba(208,160,129,1) 30%, rgba(195,101,41,1) 50%, rgba(208,160,129,1) 70%);",
+  Vol: "background-image: url('assets/vol.png')",
+  Poison: "background-image: url('assets/poison.png')",
+  Sol: "background-image: url('assets/sol.png')",
   Roche:
     "background: rgb(255,205,111);background: linear-gradient(50deg, rgba(255,205,111,1) 30%, rgba(255,174,23,1) 50%, rgba(255,205,111,1) 70%)",
-  Insecte:
-    "background: rgb(169,255,135);background: linear-gradient(50deg, rgba(169,255,135,1) 30%, rgba(15,173,0,1) 50%, rgba(169,255,135,1) 70%)",
+  Insecte: "background-image: url('assets/insecte.png')",
   Spectre:
     "background: rgb(166,95,232);background: linear-gradient(50deg, rgba(166,95,232,1) 30%, rgba(183,0,237,1) 50%, rgba(166,95,232,1) 70%)",
   Acier:
     "background: rgb(88,175,153);background: linear-gradient(50deg, rgba(88,175,153,1) 30%, rgba(114,209,215,1) 50%, rgba(88,175,153,1) 70%)",
   Feu: "background-image: url('assets/feu.png')",
-  Eau: "background: rgb(0,212,247);background: linear-gradient(50deg, rgba(0,212,247,1) 30%, rgba(0,116,255,1) 50%, rgba(0,212,247,1) 70%);",
+  Eau: "background-image: url('assets/eau.png')",
   Plante: "background-image: url('assets/plante.png')",
-  Électrik:
-    "background: rgb(221,221,221);background: linear-gradient(50deg, rgba(221,221,221,1) 30%, rgba(254,255,0,1) 50%, rgba(221,221,221,1) 70%)",
-  Psy: "background: rgb(255,49,88);background: linear-gradient(50deg, rgba(255,49,88,1) 30%, rgba(255,190,202,1) 50%, rgba(255,49,88,1) 70%)",
+  Électrik: "background-image: url('assets/electrik.png')",
+  Psy: "background-image: url('assets/psy.png')",
   Glace:
     "background: rgb(49,181,255);background: linear-gradient(50deg, rgba(49,181,255,1) 30%, rgba(190,252,255,1) 50%, rgba(49,181,255,1) 70%)",
   Dragon:
     "background: rgb(0,129,201);background: linear-gradient(50deg, rgba(0,129,201,1) 30%, rgba(2,0,163,1) 50%, rgba(0,129,201,1) 70%)",
   Ténèbres:
     "background: rgb(0,94,147);background: linear-gradient(50deg, rgba(0,94,147,1) 30%, rgba(1,0,59,1) 50%, rgba(0,94,147,1) 70%)",
-  Fée: "background: rgb(255,117,231);background: linear-gradient(50deg, rgba(255,117,231,1) 30%, rgba(255,200,246,1) 50%, rgba(255,117,231,1) 70%)",
+  Fée: "background-image: url('assets/fee.png')",
 };
 // console.log(Object.keys(typeCouleur));
 fetch("https://pokebuildapi.fr/api/v1/pokemon/limit/200")
